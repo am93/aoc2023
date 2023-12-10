14fe4a1dd1657ed7d30047f9eb40bd3ebd93f194
am93 <anzze.medved@gmail.com>
2023-12-10 10:33:28 +0100

    day 09 code minimization

--- day09/index.js
+++ day09/index.js
@@ -13,7 +13,6 @@ function partOne(filename) {
       arr.push(tempArr);
       crrIdx++;
     }
-    //console.log(arr);
     return arr.reduce((sum, a) => sum + a.pop(), 0);
   });
   return mapped.reduce((sum, val) => sum + val);
@@ -29,21 +28,16 @@ function partTwo(filename) {
       for(let idx = 0; idx < arr[crrIdx].length-1; idx++) {
         tempArr.push(arr[crrIdx][idx+1] - arr[crrIdx][idx]);
       }
-      if(!tempArr.every((x) => x == 0)){
-        arr.push(tempArr);
-      }
-      else 
-        break;
-        
+      if(!tempArr.every((x) => x == 0)) arr.push(tempArr);
+      else break;
       crrIdx++;
     }
-
-    return arr.reverse().reduce((acc, a) => {return a.shift() - acc;}, 0);
+    return arr.reverse().reduce((acc, a) => a.shift() - acc, 0);
   });
   return mapped.reduce((sum, val) => sum + val);
 }
 
 console.log('Part 1 sample:', partOne('./sample.txt'));
 console.log('Part 1       :', partOne('./input.txt'));
-//console.log('Part 2 sample:', partTwo('./sample2.txt'));
+console.log('Part 2 sample:', partTwo('./sample.txt'));
 console.log('Part 2       :', partTwo('./input.txt'));
